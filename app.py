e8309832508b3893a857dd3c439ddc6530323455
Santiago Ospina Idrobo <santiagospidrobo15@gmail.com>
2023-10-31 07:41:12 -0500

    change port

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/factorial/<int:num>')
 def factorial(num):
   if num < 0:
-    return "No se puede calcular el factorial de un número negativo."
+    return "Error: No se puede calcular el factorial de un número negativo."
   
   result = 1
   for i in range(1, num+1):
@@ -13,4 +13,4 @@ def factorial(num):
   return str(result)
 
 if __name__ == '__main__':
-  app.run(debug=True)
+  app.run(host='0.0.0.0', port=3003)
